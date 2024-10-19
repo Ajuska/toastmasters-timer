d048bb2d45f448f0f6ff0d196e1af0e5fbb62b67
Andrea Vicarova <andrea.vicarova@gmail.com>
2024-10-19 16:11:56 -0700

    Add simple timer functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrl: './app.component.sass',
 })
 export class AppComponent {
-  title = 'toastmasters-timer';
+  timer: number | NodeJS.Timeout | undefined;
+  seconds: number = 0;
+
+  startTimer() {
+    if (!this.timer) {
+      this.timer = setInterval(() => {
+        this.seconds++;
+      }, 1000);
+    }
+  }
+
+  stopTimer() {
+    if (this.timer) {
+      clearInterval(this.timer);
+      this.timer = undefined;
+    }
+  }
+
+  resetTimer() {
+    this.stopTimer();
+    this.seconds = 0;
+  }
+
+  ngOnDestroy() {
+    this.stopTimer();
+  }
 }
