485efbdd01ca4e725aca74ac6425fff7ad8e2391
Andrea Vicarova <andrea.vicarova@gmail.com>
2024-10-19 19:06:12 -0700

    Change background color based on time left in a speech

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,43 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass',
 })
 export class AppComponent {
   timer: number | NodeJS.Timeout | undefined;
   seconds: number = 0;
+  userMinutes: number = 0;
+  backgroundColor: string = 'white';
+  isDisabled: boolean = false;
+
+  timeLeftSpeech = {
+    150: 'green',
+    90: 'yellow',
+    30: 'red',
+    15: 'red', // add exlamation point? + buzz
+    0: 'red', // flash red? + 2 buzzes
+  };
+
+  colorSignifigance = {
+    green: 'green',
+    yellow: 'yellow',
+    red: 'red',
+  };
 
   startTimer() {
     if (!this.timer) {
+      this.isDisabled = true;
       this.timer = setInterval(() => {
         this.seconds++;
+        this.updateBackgroundColor();
       }, 1000);
     }
   }
@@ -22,6 +45,7 @@ export class AppComponent {
   stopTimer() {
     if (this.timer) {
       clearInterval(this.timer);
+      this.isDisabled = false;
       this.timer = undefined;
     }
   }
@@ -29,6 +53,34 @@ export class AppComponent {
   resetTimer() {
     this.stopTimer();
     this.seconds = 0;
+    this.backgroundColor = 'white';
+  }
+
+  updateBackgroundColor() {
+    const totalSeconds = this.userMinutes * 60;
+    const remainingSeconds = totalSeconds - this.seconds;
+
+    if (this.userMinutes <= 0) {
+      this.stopTimer();
+      this.seconds = 0;
+      this.backgroundColor = 'white';
+      return;
+    }
+
+    if (
+      remainingSeconds >
+      Math.max(...Object.keys(this.timeLeftSpeech).map(Number))
+    ) {
+      this.backgroundColor = 'white';
+      return;
+    }
+
+    for (const [time, color] of Object.entries(this.timeLeftSpeech)) {
+      if (remainingSeconds <= +time && this.seconds > 0) {
+        this.backgroundColor = color;
+        break;
+      }
+    }
   }
 
   ngOnDestroy() {
