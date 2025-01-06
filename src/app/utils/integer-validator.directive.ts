df91e204881fe12b0faefbe53750d0fd801bcac5
Andrea Vicarova <andrea.vicarova@gmail.com>
2025-01-05 22:53:02 -0800

    Rework validation logic to account for new text input

--- src/app/utils/integer-validator.directive.ts
+++ src/app/utils/integer-validator.directive.ts
@@ -20,13 +20,27 @@ import {
 export class IntegerValidatorDirective implements Validator {
   validate(control: AbstractControl): ValidationErrors | null {
     const value = control.value;
-    if (value === null || value === undefined) {
-      return { message: 'Enter a valid number.' };
-    } else if (value === 0) {
-      return { message: 'Please enter a number greater than 0.' };
-    } else if (value < 0) {
-      return { message: 'Please enter a positive number.' };
+
+    if (value === null || value === undefined || value === '') {
+      return { message: 'Please enter a valid number.' };
     }
+
+    if (typeof value === 'string') {
+      const trimmedValue = value.trim();
+
+      if (trimmedValue.endsWith('.') || trimmedValue.endsWith(',')) {
+        return { message: 'Trailing dots are not allowed.' };
+      }
+
+      if (isNaN(Number(trimmedValue))) {
+        return { message: 'Please enter a valid number.' };
+      }
+
+      if (parseFloat(trimmedValue) <= 0) {
+        return { message: 'Please enter a number greater than 0.' };
+      }
+    }
+
     return null;
   }
 }
